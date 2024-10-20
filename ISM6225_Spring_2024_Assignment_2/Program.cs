e24204099c8c5c80b511d8efe45e84d269f4833b
kseniia biriukova <kseniiabiriukova@usf.edu>
2024-10-20 14:27:09 -0400

    Modified program.cs

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment_2
 {
@@ -10,8 +12,11 @@ namespace Assignment_2
             // Question 1: Find Missing Numbers in Array
             Console.WriteLine("Question 1:");
             int[] nums1 = { 4, 3, 2, 7, 8, 2, 3, 1 };
+            // error checking
+            //int[] nums1 = { 4, 3, 2, 7, 8, 2, 10, -1 };
             IList<int> missingNumbers = FindMissingNumbers(nums1);
             Console.WriteLine(string.Join(",", missingNumbers));
+                        
 
             // Question 2: Sort Array by Parity
             Console.WriteLine("Question 2:");
@@ -62,13 +67,39 @@ namespace Assignment_2
         {
             try
             {
+                List<int> nums_missing = new List<int>();
                 // Write your code here
-                return new List<int>(); // Placeholder
+                int len=nums.Length;
+                foreach (var j in nums)
+                {
+                    if (j > len | j < 1)
+                    {
+                        Console.WriteLine("Array elements must be between 1 and n");
+                        return new List<int>();
+                    };
+                };
+                for (int i = 1; i < len;i++) 
+                { 
+                    
+                    if (!nums.Contains(i))
+                    { 
+                        nums_missing.Add(i);
+                    };
+                };
+                if (nums_missing.Count == 0)
+                {
+                    Console.WriteLine("No missing values");
+                };
+                return nums_missing;
+                
             }
             catch (Exception)
             {
-                throw;
+                Console.WriteLine("Exception caught!");
+                //throw;
+                return new List<int>();
             }
+            
         }
 
         // Question 2: Sort Array by Parity
@@ -77,11 +108,29 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                List<int> nums_odd = new List<int>();
+                List<int> nums_even = new List<int>();
+                
+                foreach (var j in nums)
+                {
+                    if (j %2==0)
+                    {
+                        nums_even.Add(j);
+                    }
+                    else 
+                    { 
+                        nums_odd.Add(j);
+                    }
+                };
+                nums_even.AddRange(nums_odd);
+                int[] myArray = nums_even.ToArray();
+                return myArray; // Placeholder
             }
             catch (Exception)
             {
-                throw;
+                    Console.WriteLine("Exception caught!");
+                    //throw;
+                    return new int[0];
             }
         }
 
@@ -91,11 +140,35 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return new int[0]; // Placeholder
+                
+                List<int> nums2 = new List<int>();
+                Array.Sort(nums);
+                
+                foreach (var j in nums)
+                {
+                    int k = target - j;
+                    if (nums.Contains(k) )
+                    {
+                        nums2.Add(Array.IndexOf(nums, j));
+                        nums2.Add(Array.IndexOf(nums, k));
+                        int[] myArray = nums2.ToArray();
+                        return myArray; // Placeholder
+                    };
+                };
+                if (nums2.Count == 0)
+                {
+                    Console.WriteLine("No such numbers");
+                };
+                int[] myArray2 = nums2.ToArray();
+                return myArray2;
+                
+
             }
             catch (Exception)
             {
-                throw;
+                Console.WriteLine("Exception caught!");
+                //throw;
+                return new int[0];
             }
         }
 
@@ -104,8 +177,60 @@ namespace Assignment_2
         {
             try
             {
+                List<int> nums_pos = new List<int>();
+                List<int> nums_neg = new List<int>();
+                if (nums.Length < 3) 
+                {
+                    Console.WriteLine("Array is too short!");
+                }
                 // Write your code here
-                return 0; // Placeholder
+                foreach (var j in nums)
+                {
+                    if (j >=0 )
+                    {
+                        nums_pos.Add(j);
+                    }
+                    else
+                    {
+                        nums_neg.Add(j);
+                    }
+                };
+                nums_pos.Sort();
+                nums_neg.Sort();
+                //nope
+                int a = 0;
+                int b = 0;
+                int c = 0;
+                int d = 0;
+                int e = 0;
+                int f = 0;
+                if (nums_pos.Count != 0)
+                {
+                    a = nums_pos[^1];
+                    b = nums_pos[^2];
+                    c = nums_pos[^3];
+                }
+                if (nums_neg.Count != 0)
+                {
+                    d = nums_neg[1];
+                    e = nums_neg[2];
+                    f = nums_neg[3];
+                }
+                if (a * b * c > d * e * a)
+                {
+                    return a * b * c;
+                }// Placeholder
+                else
+                {
+                    if (d*e*f > d * e * a)
+                    {
+                        return d*e*f;
+                    }
+                    else
+                    {
+                        return d * e * a;
+                    }
+                }
             }
             catch (Exception)
             {
@@ -119,7 +244,8 @@ namespace Assignment_2
             try
             {
                 // Write your code here
-                return "101010"; // Placeholder
+                string binary = Convert.ToString(decimalNumber, 2);
+                return binary; // Placeholder
             }
             catch (Exception)
             {
@@ -132,8 +258,29 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                int low = 0, high = nums.Length - 1;
+
+                while (low < high)
+                {
+                    
+                    if (nums[low] <= nums[high])
+                    {
+                        return nums[low];
+                    }
+                    
+                    int mid = (low + high) / 2;
+
+                    if (nums[mid] > nums[high])
+                    {
+                        low = mid + 1;
+                    }
+
+                    else
+                    {
+                        high = mid;
+                    }
+                }
+                return nums[low];
             }
             catch (Exception)
             {
@@ -146,8 +293,18 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return false; // Placeholder
+                
+                string str=x.ToString();
+                int len = str.Length;
+
+                for (int i = 0; i < len / 2; i++)
+                {
+
+                    if (str[i] != str[len - i - 1])
+                        return false;
+                }
+
+                return true;
             }
             catch (Exception)
             {
@@ -160,8 +317,17 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                if (n<0 | n > 30) 
+                {
+                    Console.WriteLine("Choose a number between 0 and 30");
+                    return 0; 
+                }
+                if (n == 0) return 0;
+                else if (n == 1) return 1;
+                else
+                {
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+                }
             }
             catch (Exception)
             {
